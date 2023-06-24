583706e7d2e7bf4e9a85a9fa940c7f947795ed72
sinamong0620 <sinaskim60@gmail.com>
2023-06-25 01:39:26 +0900

    양방향 바인딩
    onSubmit 이벤트에서 제출되고나서 모든 입력폼에 있는 값을 빈칸으로 만드는 작업을 해주었음.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,6 +30,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
     //브라우저는 폼이 제출될 때마다 페이지를 다시 로드한다.(기본동작)
@@ -37,7 +40,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -45,6 +52,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -52,6 +60,7 @@ const ExpenseForm = () => {
           <label>date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
