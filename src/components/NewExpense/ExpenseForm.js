38f0bb3eeae22ac9a91eb2918372cfef0bb5612f
sinamong0620 <sinaskim60@gmail.com>
2023-06-25 01:27:37 +0900

    state 다수 접근 방법 이용
    state의 다수 접근 방법을 이용하고 onsubmit을 통해 form이 제출될 때마다 기본 이벤트가 발동해서 페이지가 리로드되는 것을 event.preventDefault 함수를 통해 막았음.

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  //다수의 상태 접근 방법
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //만일 이전 상태에 의존하게 되는 것이라면 ex) setTitle((prevState)=>{return { ...prevState, enteredTitle : event.target.value }}) ...를 활용해보자.
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); //기본 요청이 보내지는 것을 막을 수 있다. 그렇기 때문에 페이지가 리로드되지 않을 것이다.
+
+    //폼이 제출될 때 객체를 만들어서 생성하는 것!
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+  return (
+    //브라우저는 폼이 제출될 때마다 페이지를 다시 로드한다.(기본동작)
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+export default ExpenseForm;
